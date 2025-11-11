0dd76beab44ba75496301cb1e62c60840044ee55
Seiya Kagaya <231023@st.yoshida-g.ac.jp>
2025-11-11 10:21:45 +0900

    一旦ファイルの整理をした。これからSDKなどの整備

---
+++ includemanager.h
@@ -0,0 +1,66 @@
+//------------------------------------------------------------
+// @file        includemanager.h
+// @brief       必要なヘッダをここに置いて一括管理
+//------------------------------------------------------------
+#ifndef _INCLUDE_MANAGE_H_
+#define _INCLUDE_MANAGE_H_
+
+
+#include <windows.h>
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+
+
+
+// 随時
+struct AnyTime
+{
+    int playerId;
+    unsigned int inputFlags;
+    DWORD timeStamp;
+};
+
+// 定期
+struct Regular
+{
+    unsigned int objectID;
+    XMFLOAT3 position;
+    XMFLOAT4 rotation;
+    XMFLOAT3 linerVelocity;
+    XMFLOAT3 angularVelocity;
+};
+
+
+
+enum ConsoleColor {
+    BLACK = 0,
+    BLUE = 1,
+    GREEN = 2,
+    CYAN = 3,
+    RED = 4,
+    MAGENTA = 5,
+    YELLOW = 6,
+    WHITE = 7,
+    GRAY = 8,
+    LIGHT_BLUE = 9,
+    LIGHT_GREEN = 10,
+    LIGHT_CYAN = 11,
+    LIGHT_RED = 12,
+    LIGHT_MAGENTA = 13,
+    LIGHT_YELLOW = 14,
+    BRIGHT_WHITE = 15
+};
+
+
+//Consoleの文字色変更
+inline void SetConsoleColor(ConsoleColor color) 
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, static_cast<WORD>(color));
+}
+
+
+
+#endif
